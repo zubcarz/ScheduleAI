f95259b4cedf4e4e184a8059a6c7c382d2f11887
zubcarz <zubcarz@gmail.com>
2017-01-03 17:42:00 -0500

    001-1 Complement initial commit

---
+++ ScheduleAI/Main.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "main.h"
+
+using FText = std::string;
+using int32 = int;
+
+int32 main()
+{
+	std::cout << "Star Program " << std::endl;
+	return 0; //exit game
+}
---
+++ ScheduleAI/Main.h
@@ -0,0 +1,3 @@
+#pragma once
+
+using FText = std::string;
